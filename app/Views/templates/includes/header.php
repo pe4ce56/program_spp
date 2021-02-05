ac85578fb159085606a384ac983fe7e6aa8fd1fe
Damai Deo <damaideo6@gmail.com>
2021-02-05 22:23:56 +0700

    starting project

---
+++ app/Views/templates/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Spica Admin</title>
+    <!-- base:css -->
+    <link rel="stylesheet" href="<?= base_url() ?>/template/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>/template/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="<?= base_url() ?>/template/css/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="<?= base_url() ?>/template/images/favicon.png" />
+</head>
+
+<body>
+
+    <div class="container-scroller d-flex">
---
+++ app/Views/templates/main.php
@@ -0,0 +1,5 @@
+<?= $this->include('templates/includes/header') ?>
+<?= $this->include('templates/includes/sidebar') ?>
+<?= $this->include('templates/includes/topbar') ?>
+    <?= $this->renderSection('content') ?>
+<?= $this->include('templates/includes/footer') ?>
